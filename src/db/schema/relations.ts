8f1dcff9e8e5a857c2fa62d82ddad4945111f02b
@油腻_w4v0 <16615812538@163.com>
2026-03-24 18:20:10 +0800

    Update relations.ts

--- src/db/schema/relations.ts
+++ src/db/schema/relations.ts
@@ -1,6 +1,7 @@
 import { relations } from "drizzle-orm";
 import { sessions } from "./sessions";
 import { users } from "./users";
+import { messages } from "./messages";
 
 export const usersRelations = relations(users, ({ many }) => ({
   sessions: many(sessions),
@@ -9,6 +10,17 @@ export const usersRelations = relations(users, ({ many }) => ({
 export const sessionsRelations = relations(sessions, ({ one }) => ({
   user: one(users, {
     fields: [sessions.userId],
+    references: [users.id],
+  }),
+}));
+
+export const messagesRelations = relations(messages, ({ one, many }) => ({
+  sessions: one(sessions, {
+    fields: [messages.sessionId],
+    references: [sessions.id],
+  }),
+  sender: one(users, {
+    fields: [messages.senderId],
     references: [users.id],
   }),
 }));
